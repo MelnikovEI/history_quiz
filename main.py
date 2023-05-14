b4124c5d7433be3b2b2e8f95a4468420dbd4c39d
MelnikovEI <64632765+MelnikovEI@users.noreply.github.com>
2023-05-14 18:49:47 +0300

    Random question sending

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import json
 import logging
+from random import randrange
+import telegram
 from environs import Env
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
@@ -22,18 +25,32 @@ def help_command(update: Update, context: CallbackContext) -> None:
 
 def echo(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    if update.message.text == 'New question':
+        with open('quiz_qna.json', 'r', encoding='utf-8') as f:
+            questions = json.load(f)
+        update.message.reply_text(questions[randrange(len(questions))]['Вопрос'])
+    else:
+        update.message.reply_text(update.message.text)
 
 
 def main() -> None:
     env = Env()
     env.read_env()
     tg_bot_token = env('TG_BOT_TOKEN')
+
     updater = Updater(tg_bot_token)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    custom_keyboard = [['New question', 'Give up'],
+                       ['My score']]
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+    bot = telegram.Bot(token=tg_bot_token)
+    chat_id = 546682970
+    bot.send_message(chat_id=chat_id, text="Custom Keyboard Test", reply_markup=reply_markup)
+
     updater.start_polling()
     updater.idle()
 
