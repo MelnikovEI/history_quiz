1921f85ccb91f40c7887084b83a23da99e18deb6
MelnikovEI <64632765+MelnikovEI@users.noreply.github.com>
2023-05-21 20:51:51 +0300

    add keyboard to vk_bot

--- vk_bot.py
+++ vk_bot.py
@@ -2,13 +2,15 @@ import random
 import vk_api as vk
 from environs import Env
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.utils import get_random_id
 
 
 def echo(event, vk_api):
     vk_api.messages.send(
         user_id=event.user_id,
         message=event.text,
-        random_id=random.randint(1,1000)
+        random_id=random.randint(1, 1000)
     )
 
 
@@ -17,10 +19,29 @@ def main() -> None:
     env.read_env()
     vk_session = vk.VkApi(token=env('VK_GROUP_TOKEN'))
     vk_api = vk_session.get_api()
+
+    """ Пример создания клавиатуры для отправки ботом """
+    keyboard = VkKeyboard(one_time=True)
+
+    keyboard.add_button('Белая кнопка', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button('Зелёная кнопка', color=VkKeyboardColor.POSITIVE)
+
+    keyboard.add_line()  # Переход на вторую строку
+    keyboard.add_button('Красная кнопка', color=VkKeyboardColor.NEGATIVE)
+
+    keyboard.add_line()
+    keyboard.add_button('Синяя кнопка', color=VkKeyboardColor.PRIMARY)
+
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             echo(event, vk_api)
+            vk_api.messages.send(
+                peer_id=event.peer_id,
+                random_id=get_random_id(),
+                keyboard=keyboard.get_keyboard(),
+                message='Пример клавиатуры'
+            )
 
 
 if __name__ == "__main__":
